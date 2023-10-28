fd338205ce1b2c0cf4335e217f0c2ae944cc9301
shubh <shubham998845@gmail.com>
2023-10-28 16:38:42 +0530

    home completed

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,15 @@
 import React from "react";
 
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
 function CustomButton({
   title,
   containerStyles,
   handleClick,
+  textStyles,
+  isDisabled,
+  rightIcon,
   btnType,
 }: CustomButtonProps) {
   return (
@@ -17,7 +21,17 @@ function CustomButton({
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles}`}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image
+            src={rightIcon}
+            alt="right icon"
+            fill
+            className="object-contain"
+          />
+        </div>
+      )}
     </button>
   );
 }
